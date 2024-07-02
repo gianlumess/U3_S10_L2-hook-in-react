d5859e5c84ff8bb9d0d5fd6a10ca5e6a47194fb6
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-02 15:00:51 +0200

    refactor bookList

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,52 +1,53 @@
-import { Component } from "react";
 import SingleBook from "./SingleBook";
 import { Col, Form, Row } from "react-bootstrap";
 import CommentArea from "./CommentArea";
+import { useState } from "react";
 
-class BookList extends Component {
-  state = {
+const BookList = (props) => {
+  /*  state = {
     searchQuery: "",
-    selectedAsin: ""
+    selectedAsin: "",
+  }; */
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedAsin, setSelectedAsin] = useState("");
+
+  const changeAsin = (newAsin) => {
+    setSelectedAsin(newAsin);
   };
 
-  changeAsin = newAsin => {
-    this.setState({ selectedAsin: newAsin });
-  };
-
-  render() {
-    return (
-      <>
-        <Row className="justify-content-center mt-5">
-          <Col xs={12} md={4} className="text-center">
-            <Form.Group>
-              <Form.Control
-                type="search"
-                placeholder="Cerca un libro"
-                value={this.state.searchQuery}
-                onChange={e => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="g-2 mt-3">
-          <Col md={8}>
-            <Row>
-              {this.props.books
-                .filter(b => b.title.toLowerCase().includes(this.state.searchQuery.toLowerCase()))
-                .map(b => (
-                  <Col xs={12} md={3} key={b.asin}>
-                    <SingleBook book={b} changeAsin={this.changeAsin} selectedAsin={this.state.selectedAsin} />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          <Col md={4}>
-            <CommentArea asin={this.state.selectedAsin} />
-          </Col>
-        </Row>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} md={4} className="text-center">
+          <Form.Group>
+            <Form.Control
+              type="search"
+              placeholder="Cerca un libro"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="g-2 mt-3">
+        <Col md={8}>
+          <Row>
+            {props.books
+              .filter((b) => b.title.toLowerCase().includes(searchQuery.toLowerCase()))
+              .map((b) => (
+                <Col xs={12} md={3} key={b.asin}>
+                  <SingleBook book={b} changeAsin={changeAsin} selectedAsin={selectedAsin} />
+                </Col>
+              ))}
+          </Row>
+        </Col>
+        <Col md={4}>
+          <CommentArea asin={selectedAsin} />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default BookList;
