8abb534145747c4a5823f9518b01d43f87a974d6
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-02 15:12:38 +0200

    refactor singleBook

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,33 +1,32 @@
-import { Component } from "react";
 import { Card } from "react-bootstrap";
 // import CommentArea from "./CommentArea";
 
-class SingleBook extends Component {
+const SingleBook = (props) => {
   // state = {
   //   selected: false
   // };
 
-  render() {
-    return (
-      <Card style={{ border: this.props.book.asin === this.props.selectedAsin ? "3px solid red" : "3px solid #ebebeb" }}>
-        <Card.Img
-          variant="top"
-          src={this.props.book.img}
-          onClick={() => {
-            // devo modificare lo stato nel livello superiore
-            // ora abbiamo una prop che contiene la funzione in grado di modificare lo stato al livello superiore! (changeAsin)
-            this.props.changeAsin(this.props.book.asin);
-            // this.setState({ selected: !this.state.selected });
-          }}
-        />
-        <Card.Body>
-          <Card.Title style={{ color: "black" }}>{this.props.book.title}</Card.Title>
+  /*  const [selected, setSelected] = useState(false); */
 
-          {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
-        </Card.Body>
-      </Card>
-    );
-  }
-}
+  return (
+    <Card style={{ border: props.book.asin === props.selectedAsin ? "3px solid red" : "3px solid #ebebeb" }}>
+      <Card.Img
+        variant="top"
+        src={props.book.img}
+        onClick={() => {
+          // devo modificare lo stato nel livello superiore
+          // ora abbiamo una prop che contiene la funzione in grado di modificare lo stato al livello superiore! (changeAsin)
+          props.changeAsin(props.book.asin);
+          // this.setState({ selected: !this.state.selected });
+        }}
+      />
+      <Card.Body>
+        <Card.Title style={{ color: "black" }}>{props.book.title}</Card.Title>
+
+        {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default SingleBook;
